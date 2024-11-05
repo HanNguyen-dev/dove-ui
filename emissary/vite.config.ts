59b143212683baa1092bd30f55c3140a1fa3f6ee
Han Nguyen <han.nguyen@outlook.com>
2024-11-04 17:37:32 -0800

    migrate from CRA to vite

---
+++ emissary/vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config
+export default defineConfig({
+  base: "/",
+  plugins: [react()],
+  server: {
+    open: false,
+    port: 3000,
+    proxy: {
+      "/jobs": "",
+    },
+  },
+});
